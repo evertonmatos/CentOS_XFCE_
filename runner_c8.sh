0d49d9bdba6d58d5b4149d15d5ef9dc252f1de0c
matos_everton <matos.everton@gmail.com>
2022-02-21 20:09:26 -0300

    Changing ansible order installation.

--- runner_c8.sh
+++ runner_c8.sh
@@ -6,7 +6,6 @@
 ip_destino=$1
 root_password=$2
 
-sudo yum install ansible -y
 ssh-keygen -R root@$ip_destino
 ssh-keyscan $ip_destino >> ~/.ssh/known_hosts
 echo yes | ssh-copy-id root@$ip_destino
@@ -14,6 +13,8 @@ rm -rf /tmp/CentOS_XFCE.zip
 zip -r /tmp/CentOS_XFCE.zip .
 ssh root@$ip_destino rm -rf /CentOS_XFCE
 scp /tmp/CentOS_XFCE.zip root@$ip_destino:/tmp/CentOS_XFCE.zip
+sudo yum install ansible -y
+sudo apt install ansible -y
 ansible-playbook -i root@$ip_destino, --extra-vars "ansible_sudo_pass=$root_password" ansible/centos8/00_unzip_project.yml
 ansible-playbook -i root@$ip_destino, --extra-vars "ansible_sudo_pass=$root_password" ansible/centos8/01_setting_permissions.yml
 ansible-playbook -i root@$ip_destino, --extra-vars "ansible_sudo_pass=$root_password" ansible/centos8/02_update_system.yml
